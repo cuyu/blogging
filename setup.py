de28451660c94d0b1bdf7daf444a3a4045427930
cuyu <icyarm@qq.com>
2017-08-07 17:25:35 +0800

    Change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import setuptools
-from src import __VERSION__
+from blogging import __VERSION__
 from setuptools.command.install import install
 import os
 
