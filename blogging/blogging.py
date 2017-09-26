95f00f0d2135ec643c66a722833abc9d2bcf96df
cuyu <icyarm@qq.com>
2017-09-26 16:46:56 +0800

    Print html of the image with width info when using image cmd. Disable the argcomplete patch as it fails in some situations

--- blogging/blogging.py
+++ blogging/blogging.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import argcomplete_patch
+# import argcomplete_patch
 import argparse
 import cgi
 import os
@@ -14,6 +14,72 @@ import argcomplete
 import time
 from termcolor import colored
 from constants import __VERSION__, BLOGGING_SETTINGS_FILE
+import struct
+
+
+class UnknownImageFormat(Exception):
+    pass
+
+
+def get_image_size(file_path):
+    """
+    Return (width, height) for a given img file content - no external
+    dependencies except the os and struct modules from core
+    """
+    size = os.path.getsize(file_path)
+
+    with open(file_path) as input:
+        height = -1
+        width = -1
+        data = input.read(25)
+
+        if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+            # GIFs
+            w, h = struct.unpack("<HH", data[6:10])
+            width = int(w)
+            height = int(h)
+        elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
+              and (data[12:16] == 'IHDR')):
+            # PNGs
+            w, h = struct.unpack(">LL", data[16:24])
+            width = int(w)
+            height = int(h)
+        elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+            # older PNGs?
+            w, h = struct.unpack(">LL", data[8:16])
+            width = int(w)
+            height = int(h)
+        elif (size >= 2) and data.startswith('\377\330'):
+            # JPEG
+            msg = " raised while trying to decode as JPEG."
+            input.seek(0)
+            input.read(2)
+            b = input.read(1)
+            try:
+                while (b and ord(b) != 0xDA):
+                    while (ord(b) != 0xFF): b = input.read(1)
+                    while (ord(b) == 0xFF): b = input.read(1)
+                    if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                        input.read(3)
+                        h, w = struct.unpack(">HH", input.read(4))
+                        break
+                    else:
+                        input.read(int(struct.unpack(">H", input.read(2))[0]) - 2)
+                    b = input.read(1)
+                width = int(w)
+                height = int(h)
+            except struct.error:
+                raise UnknownImageFormat("StructError" + msg)
+            except ValueError:
+                raise UnknownImageFormat("ValueError" + msg)
+            except Exception as e:
+                raise UnknownImageFormat(e.__class__.__name__ + msg)
+        else:
+            raise UnknownImageFormat(
+                "Sorry, don't know how to get information from this file."
+            )
+
+    return width, height
 
 
 def validate_settings():
@@ -305,6 +371,7 @@ date: {date}
     elif args.command == 'image':
         image_extension = args.image_path.split('.')[-1]
         image_name = args.draft_file.replace('.md', '') + '.' + image_extension
+        image_width, image_height = get_image_size(args.image_path)
         index = 1
         while os.path.isfile(os.path.join(SETTINGS.PROJECT_PATH, SETTINGS.IMAGES_FOLDER, image_name)):
             image_name = args.draft_file.replace('.md', '') + '-{0}'.format(index) + '.' + image_extension
@@ -312,7 +379,9 @@ date: {date}
         call(['mv', args.image_path, os.path.join(SETTINGS.PROJECT_PATH, SETTINGS.IMAGES_FOLDER, image_name)])
         os.chdir(SETTINGS.PROJECT_PATH)
         call(['git', 'add', os.path.join(SETTINGS.PROJECT_PATH, SETTINGS.IMAGES_FOLDER, image_name)])
-        print '/{0}/{1}'.format(SETTINGS.IMAGES_FOLDER, image_name)
+        print '<img title="{0}" src="/{1}/{2}" width="{3}" />'.format(image_name, SETTINGS.IMAGES_FOLDER, image_name,
+                                                                      image_width)
+        print '<span class="caption">REPLACEME</span>'
         # TODO: add image path to clipboard
     elif args.command == 'edit':
         post_path = os.path.join(SETTINGS.PROJECT_PATH, SETTINGS.POSTS_FOLDER, args.post_file)
